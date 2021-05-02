4880373db0561a3470b7ee247f12df994673e393
JorgeMarinC <jorgemarincordero@gmail.com>
2021-05-02 02:39:19 +0200

    Add data edition components

---
+++ src/front/foundsResearchSourcesStatsGUI/FoundsResearchSourcesStatsDataEdit.svelte
@@ -0,0 +1,110 @@
+<script>
+    import {onMount} from "svelte";
+    import {pop} from "svelte-spa-router"
+    import Table from "sveltestrap/src/Table.svelte";
+    import Button from "sveltestrap/src/Button.svelte";
+
+    const API_PATH = "/api/v1/foundsresearchsources-stats";
+
+    export let params = {};
+
+    let percentageOfGovernmentFunding = null;
+    let percentageOfPrivateFinancing = null;
+    let percentageOfNonProfitFunding = null;
+
+
+    // Retrieve data from DB
+    async function getData() {
+    console.log("Fetching data...");
+    const res = await fetch(API_PATH + "/" + params.country + "/" + params.year);
+        
+    if (res.ok) {
+        const json = await res.json();
+        percentageOfGovernmentFunding = json.percentage_of_government_funding;
+        percentageOfPrivateFinancing = json.percentage_of_private_financing;
+        percentageOfNonProfitFunding = json.percentage_of_non_profit_funding;
+        console.log("Received data.");
+    } else {
+        console.log("Error");
+    }
+    }
+    onMount(getData);
+
+    // Update data on DB
+    async function updateData() {
+        console.log("Updating data...");
+    const res = await fetch(API_PATH + "/" + params.country + "/" + params.year, {
+        method: "PUT",
+        body: JSON.stringify({
+            country: params.country,
+            year: parseInt(params.year),
+            percentage_of_government_funding: parseFloat(
+                percentageOfGovernmentFunding
+            ),
+            percentage_of_private_financing: parseFloat(
+                percentageOfPrivateFinancing
+            ),
+            percentage_of_non_profit_funding: parseFloat(
+                percentageOfNonProfitFunding
+            ),
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then((res) =>{
+                    if(res.ok){
+                        alert("El registro ha sido insertado correctamente.");
+                        getData();
+                    }
+                    if(res.status == 400){
+                        alert("La entrada de datos no es correcta:\nTodos los campos menos el país deben ser númericos.");
+                    }
+                    if(res.status == 409) {
+                        alert("Los datos que intenta introducir ya se encuentran en la base de datos.");
+                        }
+                    });
+
+
+
+    }
+
+</script>
+<main>
+    
+    <h2>Editar los datos de "{params.country}" en el año "{params.year}"</h2>
+    <Table bordered responsive>
+        <thead>
+            <tr>
+                <th>País</th>
+                <th>Año</th>
+                <th>Porcentaje financiación estatal</th>
+                <th>Porcentaje financiación privado</th>
+                <th>Porcentaje financiación ongs</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+                <tr>
+                    <td>{params.country}</td>
+                    <td>{params.year}</td>
+                    <td><input bind:value={percentageOfGovernmentFunding}></td>
+                    <td><input bind:value={percentageOfPrivateFinancing}></td>
+                    <td><input bind:value={percentageOfNonProfitFunding}></td>
+                    <td><Button color="warning" on:click={updateData(params)}>Actualizar</Button>
+                    </td>
+                </tr>
+            
+        </tbody>
+    </Table>
+    <div class="container" >
+        <div class="text-center">
+        <Button color="secondary" on:click="{pop}">Volver</Button>
+    </div>
+</div>
+</main>
+<style>
+    h2
+    {text-align: center;
+
+    }
+</style>
---
+++ src/front/foundsResearchSourcesStatsGUI/FoundsResearchSourcesStatsDataMultipleEdit.svelte
@@ -0,0 +1,118 @@
+<script>
+    import Table from "sveltestrap/src/Table.svelte";
+    import Button from "sveltestrap/src/Button.svelte";
+    import {onMount} from "svelte";
+    import {pop} from "svelte-spa-router";
+    
+    let foundsResearchSourcesData = [];
+    let updatedData = {
+        country: "",
+        year: "",
+        percentage_of_government_funding: "",
+        percentage_of_private_financing: "",
+        percentage_of_non_profit_funding: "",
+    };
+     // Retrieves all data from DB
+     async function getData() {
+        console.log("fetchong data");
+        const res = await fetch("/api/v1/foundsresearchsources-stats");
+        if (res.ok) {
+            console.log("Ok");
+            const returnedJson = await res.json();
+            foundsResearchSourcesData = returnedJson;
+            console.log(
+                "We have " + foundsResearchSourcesData.length + " registers."
+            );
+        } else {
+            console.log("Error");
+        }
+    }
+
+    // Update data on database
+    async function updateData(row) {
+        console.log("Updating data...");
+        if(confirm("¿Está seguro de querer actualizar este registro?\nRecuerde que esta acción es irreversible")){
+            const res = await fetch("/api/v1/foundsresearchsources-stats/"+row.country+"/"+row.year, {
+                method: "PUT",
+                body: JSON.stringify({
+                    country: row.country,
+                    year: parseInt(row.year),
+                    percentage_of_government_funding: parseFloat(
+                        row.percentage_of_government_funding
+                    ),
+                    percentage_of_private_financing: parseFloat(
+                        row.percentage_of_private_financing
+                    ),
+                    percentage_of_non_profit_funding: parseFloat(
+                        row.percentage_of_non_profit_funding
+                    ),
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }).then((res) =>{
+                            if(res.ok){
+                                alert("El registro ha sido insertado correctamente.");
+                                getData();
+                            }
+                            if(res.status == 400){
+                                alert("La entrada de datos no es correcta:\nTodos los campos menos el país deben ser númericos.");
+                            }
+                            if(res.status == 409) {
+                                alert("Los datos que intenta introducir ya se encuentran en la base de datos.");
+                                }
+
+                    
+                            });
+        }
+        
+    }
+
+    onMount(getData);
+</script>
+
+<main>
+        <Table bordered responsive>
+            <thead>
+                <tr>
+                    <th>País</th>
+                    <th>Año</th>
+                    <th>Porcentaje financiación estatal</th>
+                    <th>Porcentaje financiación privado</th>
+                    <th>Porcentaje financiación ongs</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each foundsResearchSourcesData as row}
+                    <tr>
+                        <td>{row.country}</td>
+                        <td>{row.year}</td>
+                        <td><input bind:value={row.percentage_of_government_funding}></td>
+                        <td><input bind:value={row.percentage_of_private_financing}></td>
+                        <td><input bind:value={row.percentage_of_non_profit_funding}></td>
+                        <td><Button color="warning" on:click={updateData(row)}>Actualizar</Button>
+                        </td>
+                    </tr>
+                {/each}
+            </tbody>
+        </Table>
+        <div class="container" >
+            <div class="text-center">
+            <Button color="secondary" on:click="{pop}">Volver</Button>
+        </div>
+    </div>
+</main>
+
+<style>
+    th,
+    td {
+        text-align: center;
+    }
+    thead {
+        font-weight: bold;
+    }
+    .text-center {
+  text-align: center;
+}
+</style>
