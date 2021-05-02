ec992a0b0c51e4f4a5e7f41b0c48a6f1d5662fa5
JorgeMarinC <jorgemarincordero@gmail.com>
2021-05-02 02:53:42 +0200

    Minimal fix on About component

--- src/front/AboutPage.svelte
+++ src/front/AboutPage.svelte
@@ -1,8 +1,10 @@
 
-       <main>
-       <p class="lead">Página informativa del grupo 20 de la asignatura, Sistemas Orientados a Servicios</p>
+       <div class="container">
 
-        <h3>Integrantes del equipo:</h3>
+        <h4>Descripción del proyecto:</h4>
+        <p>Nuestro proyecto busca la relación existente entre la capacidad de producción de energía renovable, la emisión de gases de efecto invernadero y la proveniencia de los fondos dedicados
+            a la investigacíon y desarrollo.</p>
+        <h4>Integrantes del equipo:</h4>
 
         <ul>
             <li>Jorge Marín Cordero, enlace a la cuenta de github: <a
@@ -13,19 +15,17 @@
                     href="https://github.com/jesgueada">https://github.com/jesgueada</a></li>
         </ul>
 
-        <h3>Descripción:</h3>
-        <p>Nuestro proyecto busca la relación existente entre la capacidad de producción de energía renovable, la emisión de gases de efecto invernadero y la proveniencia de los fondos dedicados
-            a la investigacíon y desarrollo.</p>
+        
 
-        <h3>URL del repositorio del proyecto:</h3>
+        <h4>URL del repositorio del proyecto:</h4>
         <p><strong>SOS2021-20</strong> (<a
                 href="https://github.com/gti-sos/SOS2021-20">https://github.com/gti-sos/SOS2021-20</a>)</p>
 
-        <h3>Deployment URL:</h3>
+        <h4>URL del despliegue:</h4>
         <p><a href="https://sos2021-20.herokuapp.com">https://sos2021-20.herokuapp.com</a></p>
 
 
-        <h3>APIs (Back-End)</h3>
+        <h4>APIs (Back-End)</h4>
         <ul>
             <li>Enlace a la API: <a
                     href="https://sos2021-20.herokuapp.com/api/v1/foundsresearchsources-stats">/foundsresearchsources-stats</a>
@@ -41,7 +41,7 @@
                     href="https://documenter.getpostman.com/view/14978263/TzJu8wiQ">Documentación</a>)</li>
         </ul>
 
-        <h3>GUIs (Front-End)</h3>
+        <h4>GUIs (Front-End)</h4>
         <ul>
             <li>Enlace a la interfaz de usuario: <a href="#/foundsresearchsources-stats">/foundsresearchsources-stats</a>
                 (Desarrollado por: Jorge Marín Cordero)</li>
@@ -50,4 +50,4 @@
             <li>Enlace a la interfaz de usuario: <a href="#/greenhousegasemissions-stats">/greenhousegasemissions-stats</a>
                 (Desarrollado por: Jesús Guerra Adame)</li>
         </ul>
-    </main>
+    </div>
