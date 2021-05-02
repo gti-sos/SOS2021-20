f19774fdeff2364ceaac80dceeae5c3abd5af781
JorgeMarinC <jorgemarincordero@gmail.com>
2021-05-02 02:40:10 +0200

    Add new functionalities

--- src/front/foundsResearchSourcesStatsGUI/FoundsResearchSourcesStats.svelte
+++ src/front/foundsResearchSourcesStatsGUI/FoundsResearchSourcesStats.svelte
@@ -1,61 +1,190 @@
 <script>
     import { onMount } from "svelte";
+    import {push} from "svelte-spa-router";
     import Table from "sveltestrap/src/Table.svelte";
-
+    import Button from "sveltestrap/src/Button.svelte";
+    const API_PATH = "/api/v1/foundsresearchsources-stats";
     let foundsResearchSourcesData = [];
+    let newData = {
+        country: "",
+        year: "",
+        percentage_of_government_funding: "",
+        percentage_of_private_financing: "",
+        percentage_of_non_profit_funding: "",
+    };
+
+    // Load the database with the initial data
+    async function loadData(){
+        console.log("Loading data...");
+        const res = await fetch(API_PATH+"/loadInitialData").then((res)=>{
+        if (res.ok) {
+            getData();
+        } else {
+            alert("Los datos que intenta introducir ya se encuentran cargados en la base de datos.");
+            console.log("Error");
+        }
+        });
+
+    }
 
     // Retrieves all data from DB
     async function getData() {
-        console.log("fetchong data");
-        const res = await fetch("/api/v1/foundsresearchsources-stats");
+        console.log("Fetching data...");
+        const res = await fetch(API_PATH);
         if (res.ok) {
-            console.log("Ok");
             const returnedJson = await res.json();
             foundsResearchSourcesData = returnedJson;
-            console.log(
-                "We have " + foundsResearchSourcesData.length + " registers."
-            );
+            console.log("!"+foundsResearchSourcesData.length + ", Registers loaded!");
         } else {
             console.log("Error");
         }
     }
-    console.log("Before call");
     onMount(getData);
-    console.log("After call");
+
+    // Insert new data on database
+    async function insertNewData() {
+        if (newData.country == "" || newData.year == "" ||
+             newData.country == null || newData.year == null){
+             alert("Debe indicar un país y un año");
+         }else{        
+                console.log("Inserting new data...");
+                const res = await fetch(API_PATH, {
+                    method: "POST",
+
+                    body: JSON.stringify({
+                                country: newData.country,
+                                year: parseInt(newData.year),
+                                percentage_of_government_funding: parseFloat(
+                                    newData.percentage_of_government_funding
+                                ),
+                                percentage_of_private_financing: parseFloat(
+                                    newData.percentage_of_private_financing
+                                ),
+                                percentage_of_non_profit_funding: parseFloat(
+                                    newData.percentage_of_non_profit_funding
+                                ),
+                            }),
+
+                    headers: {
+                        "Content-Type": "application/json",
+                            },
+
+                }).then((res) =>{
+                                    if(res.ok){
+                                        alert("El registro ha sido insertado correctamente.");
+                                        getData();
+                                    }
+                                    if(res.status == 400){
+                                        alert("La entrada de datos no es correcta:\nTodos los campos menos el país deben ser númericos.");
+                                        }
+
+                                    if(res.status == 409) {
+                                        alert("Los datos que intenta introducir ya se encuentran en la base de datos.");
+                                        }
+
+                    
+                                });
+                }    
+    }
+
+    // Delete specific data on database
+    async function deleteData(row) {
+        if(confirm("¿Está seguro de querer eliminar este registro la base de datos?\n Recuerde que esta acción es irreversible.")){
+        console.log("Deleting data...");
+        const res = await fetch(
+            API_PATH + "/" +
+                row.country +
+                "/" +
+                row.year,
+            {
+                method: "DELETE",
+            }
+        ).then((res) => {
+            getData();
+        });
+    }
+    }
+
+     // Delete all data on database
+     async function deleteAllData() {
+        if(confirm("¿Está seguro de querer eliminar todos los datos que existen en la base de datos?\n Recuerde que esta acción es irreversible.")){ 
+        console.log("Deleting all data...");
+        const res = await fetch(
+            API_PATH,
+            {
+                method: "DELETE",
+            }
+        ).then((res)=>{
+        if (res.ok) {
+            foundsResearchSourcesData = [];
+            getData();
+        } else {
+            console.log("Error");
+        }
+    });
+}
+    }
+
+    
 </script>
 
-<main>
-    <body>
+
+        <div class="container" >
+            <div class="buttons-center">
+                <Button color="success" on:click={loadData}>Cargar Datos</Button>
+                <Button color="primary" on:click={() => push("#/foundsresearchsources-stats/update-data")} >Editar Datos </Button>
+                <Button color="danger" on:click={deleteAllData}>Borrar Datos</Button>
+        </div>
+    </div>
         <Table bordered responsive>
             <thead>
                 <tr>
-                    <td>País</td>
-                    <td>Año</td>
-                    <td>Porcentaje financiación estatal</td>
-                    <td>Porcentaje financiación privado</td>
-                    <td>Porcentaje financiación ongs</td>
+                    <th>País</th>
+                    <th>Año</th>
+                    <th>Porcentaje financiación estatal</th>
+                    <th>Porcentaje financiación privado</th>
+                    <th>Porcentaje financiación ongs</th>
+                    <th>Acciones</th>
                 </tr>
             </thead>
             <tbody>
+                <tr>
+                    <td><input bind:value={newData.country} /></td>
+                    <td><input bind:value={newData.year} /></td>
+                    <td><input bind:value={newData.percentage_of_government_funding}/></td>
+                    <td><input bind:value={newData.percentage_of_private_financing}/></td>
+                    <td><input bind:value={newData.percentage_of_non_profit_funding}/></td>
+                    <td><Button color="success" on:click={insertNewData}>Añadir dato</Button></td>
+                </tr>
                 {#each foundsResearchSourcesData as row}
                     <tr>
-                        <td>{row.country}</td>
+                        <td>
+                            <a title="Edit data for {row.country} on year {row.year}" 
+                                href="#/foundsresearchsources-stats/{row.country}/{row.year}">{row.country}
+                            </a>
+                        </td>
                         <td>{row.year}</td>
                         <td>{row.percentage_of_government_funding}</td>
                         <td>{row.percentage_of_private_financing}</td>
                         <td>{row.percentage_of_non_profit_funding}</td>
+                        <td><Button color="danger" on:click={deleteData(row)}>Borrar</Button>
+                        </td>
                     </tr>
                 {/each}
             </tbody>
         </Table>
-    </body>
-</main>
+
 
 <style>
-    td {
+    th,td {
         text-align: center;
     }
     thead {
         font-weight: bold;
     }
+    .buttons-center {
+        text-align: center;
+        padding-top: 1em;
+        padding-bottom: 2em;
+}
 </style>
