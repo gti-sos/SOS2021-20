a338c21401c8098dd1fd81daa006cd1bf0a39848
jesgueada <jesusguerra34@gmail.com>
2021-05-25 01:00:15 +0200

    proxy for another API

--- src/back/greenhouseGasEmissionsAPI/v2/integration/index.js
+++ src/back/greenhouseGasEmissionsAPI/v2/integration/index.js
@@ -329,7 +329,7 @@ module.exports.init = (app) => {
 
     });
 
-//Servidor proxy integración grupo 23 HDI-stats
+//Servidor proxy para integración con grupo 23 HDI-stats
 
    app.use("/v1/integration/hdi-stats", function(req, res) {
         var apiServerHost = "https://sos2021-23.herokuapp.com/api/v1/hdi-stats";
@@ -338,6 +338,18 @@ module.exports.init = (app) => {
         console.log(`piped: ${req.url} -> ${url}`);
         req.pipe(request(url)).pipe(res);
       });
+
+//Servidor proxy para integración con grupo 04 poverty_risks
+
+      app.use("/v1/integration/poverty_risks", function(req, res) {
+        var apiServerHost = "https://sos2021-04.herokuapp.com/api/v1/poverty_risks";
+    
+        var url = apiServerHost + req.url;
+        console.log(`piped: ${req.url} -> ${url}`);
+        req.pipe(request(url)).pipe(res);
+      });
+
+      
     
 };
 
