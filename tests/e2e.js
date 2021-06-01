d8b54589a86800221a890c659b52678a9f946cef
JorgeMarinC <jorgemarincordero@gmail.com>
2021-06-01 20:20:20 +0200

    *

--- tests/e2e.js
+++ tests/e2e.js
@@ -32,52 +32,6 @@ const puppeteer = require('puppeteer');
   console.log("Loaded elements correctly? " + loaded  );
   await browser.close();
   console.log("Browser closed");
-
-
-  //--- RENEWABLE POWERCAPACITIES --
-/*
-//RENEWABLE - ABRO PAGINA PRINCIPAL 
-await page.goto('http://localhost:10000');
-    
-//PRIMERA FOTO
-await page.screenshot({ path: 'tests/renewable_1.png' });
-console.log("Pagina Abierta - Foto 1 capturada correctamente");
-
-//RENEWABLE - ABRO DESCRIPCION GRUPO 
-const [response2] = await Promise.all([
-page.waitForNavigation(),
-page.click("body > div > div > button"),
-]); 
-
-//SEGUNDA FOTO
-await page.screenshot({path: 'tests/renewable_2.png'})
-console.log("Pagina Abierta - Foto 2 capturada correctamente");
-
-//RENEWABLE - ABRO PAGINA API RENEWABLEPOWERCAPACITIES    
-await page.goto('http://localhost:10000/#/renewablepowercapacities-stats');
-await page.waitForTimeout(1000);
-
-//TERCERA FOTO
-await page.screenshot({ path: 'tests/renewable_3.png' });
-console.log("Pagina Abierta - Foto 3 capturada correctamente");
-
-//VER DATOS CARGADOS API RENEWABLEPOWERCAPACITIES
-var rowCount = (await page.$$("body > main > div.table-responsive > table > tbody > tr")).length;
-
-if(rowCount >=2 ){
-
-console.log("DATOS CARGADOS CORRECTAMENTE");
-
-await page.screenshot({ path: 'tests/renewable_4.png' });
-console.log("Datos cargados - Foto 4 capturada correctamente");
-
-}else{
-console.log("Error - No se ha cargado 2 datos o mas.");
-}
-*/
-
-//----- FIN RENEWABLEPOWERCAPACITIES ---
-
-process.exit(0);
+  process.exit(0);
 
 })();
