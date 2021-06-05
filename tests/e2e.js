37fc48ab627cec27ae0f87b09ccbd3074b16352f
JorgeMarinC <jorgemarincordero@gmail.com>
2021-06-05 03:54:11 +0200

    close #57 end to end frontend test #fix

--- tests/e2e.js
+++ tests/e2e.js
@@ -1,19 +1,16 @@
 const puppeteer = require('puppeteer');
-
 const path = require('path');
 const appDir = path.dirname(require.main.filename);
 
 (async () => {
 
+  //-------------------- MAIN APP -------------------//
   const browser = await puppeteer.launch();
   console.log("Browser opened...");
   const page = await browser.newPage();
   await page.goto('http://localhost:10000');
   console.log("Page opened! Taking a screenshot...");
-  await page.screenshot({ path: appDir+'/screenshots/01.png' });
-
-
-
+  await page.screenshot({ path: appDir+'/screenshots/main-app-01.png' });
   const [response] = await Promise.all([
     page.waitForNavigation(), 
     page.click('button.btn:nth-child(5)'), 
@@ -21,64 +18,65 @@ const appDir = path.dirname(require.main.filename);
   console.log("Clicked \"Integraciones\" link, waiting page load");
   await page.waitForTimeout(1000);
   console.log("Page opened! Taking a screenshot...");
-  await page.screenshot({ path: appDir+'/screenshots/02.png' });
+  await page.screenshot({ path: appDir+'/screenshots/main-app-02.png' });
+  //---------------------------------------------------//
 
+  //------------- FOUNDS RESEARCH SOURCES -------------//
   await page.goto('http://localhost:10000/#/foundsresearchsources-stats');
   await page.waitForTimeout(1000);
   console.log("Page opened! Taking a screenshot...");
-  await page.screenshot({ path: appDir+'/screenshots/03.png' });
+  await page.screenshot({ path: appDir+'/screenshots/founds-research-01.png' });
 
   var rowCount = (await page.$$(".table > tbody > tr")).length;
   var loaded = parseInt(rowCount) > 2;
   console.log("Loaded elements correctly? " + loaded  );
-  await browser.close();
-  console.log("Browser closed");
+
+  //---------------------------------------------------//
 
    //--- RENEWABLE POWERCAPACITIES --
 
-//RENEWABLE - ABRO PAGINA PRINCIPAL 
-/*const browser2 = await puppeteer.launch();
-console.log("Browser opened...");
-const page2 = await browser2.newPage();
-await page2.goto('http://localhost:10000');
+// RENEWABLE - ABRO PAGINA PRINCIPAL 
+await page.goto('http://localhost:10000');
     
-//PRIMERA FOTO
-await page2.screenshot({ path: appDir+'/screenshots/renewable_1.png' });
+// PRIMERA FOTO
+await page.screenshot({ path: appDir+'/screenshots/renewable-01.png' });
 console.log("Pagina Abierta - Foto 1 capturada correctamente");
 
-//RENEWABLE - ABRO DESCRIPCION GRUPO 
+// RENEWABLE - ABRO DESCRIPCION GRUPO 
 const [response2] = await Promise.all([
-page2.waitForNavigation(),
-page2.click("body > div > div > button"),
+page.waitForNavigation(),
+page.click("body > div > div > button"),
 ]); 
 
 //SEGUNDA FOTO
-await page2.screenshot({path: appDir+'/screenshots/renewable_2.png'})
+await page.screenshot({path: appDir+'/screenshots/renewable-02.png'})
 console.log("Pagina Abierta - Foto 2 capturada correctamente");
 
 //RENEWABLE - ABRO PAGINA API RENEWABLEPOWERCAPACITIES    
-await page2.goto('http://localhost:10000/#/renewablepowercapacities-stats');
-await page2.waitForTimeout(1000);
+await page.goto('http://localhost:10000/#/renewablepowercapacities-stats');
+await page.waitForTimeout(1000);
 
 
 //VER DATOS CARGADOS API RENEWABLEPOWERCAPACITIES
-var rowCount = (await page2.$$("body > main > div.table-responsive > table > tbody > tr")).length;
+var rowCount = (await page.$$("body > main > div.table-responsive > table > tbody > tr")).length;
 
 if(rowCount >=2 ){
 
 console.log("DATOS CARGADOS CORRECTAMENTE");
 
-await page2.screenshot({ path: appDir+'/screenshots/renewable_3.png' });
+await page.screenshot({ path: appDir+'/screenshots/renewable-03.png' });
 console.log("Datos cargados - Foto 3 capturada correctamente");
 
 }else{
 console.log("Error - No se ha cargado 2 datos o mas.");
 }
 
-*/
+
 //----- FIN RENEWABLEPOWERCAPACITIES ---
 
 
+ 
+  await browser.close();
+  console.log("Browser closed");
   process.exit(0);
-
 })();
