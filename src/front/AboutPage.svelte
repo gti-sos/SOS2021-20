3e9df0122fb229d7ed241c90a4b478c54a7796c0
jesgueada <jesusguerra34@gmail.com>
2021-06-07 03:48:15 +0200

    add videos to front

--- src/front/AboutPage.svelte
+++ src/front/AboutPage.svelte
@@ -4,11 +4,13 @@
 
     <div class="row videos">
         <div class="col-4">
-            <h5>Video presentación .......</h5>
+            <h5>Video presentación foundsreserachsources-stats</h5>
             <div class="embed-responsive embed-responsive-16by9 ">
                 <iframe
                     title="v1"
                     class="embed-responsive-item"
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     src="https://www.youtube.com/embed/xbhCPt6PZIU"
                     allowfullscreen
                 />
@@ -22,30 +24,33 @@
             </p>
         </div>
         <div class="col-4">
-            <h5>Video presentación .......</h5>
+            <h5>Video presentación greenhousegasemissions-stats</h5>
             <div class="embed-responsive embed-responsive-16by9 ">
                 <iframe
                     title="v1"
                     class="embed-responsive-item"
-                    src="https://www.youtube.com/embed/hTWKbfoikeg"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    src="https://www.youtube.com/embed/xVzqvtWjUyY"
                     allowfullscreen
                 />
             </div>
             <p>
-                Contrary to popular belief, Lorem Ipsum is not simply random
-                text. It has roots in a piece of classical <br />
-                Latin literature from 45 BC, making it over 2000 years old. Richard
-                McClintock, a Latin professor at Hampden-Sydney College in Virginia,<br
+                En este vídeo se puede ver un repaso general del funcionamiento de la API desarrollada por el grupo 20 de SOS-2021. En concreto nos centramos en la parte individual desarrollada por Jesús Guerra Adame, la cual muestra datos sobre las emisiones de gases de efecto invernadero y su representación gráfica. 
+                Por otra parte también se muestran los usos e integraciones realizadas.<br
                 />
             </p>
+            <h6>Creado por <a href="https://github.com/jesgueada">Jesús Guerra Adame</a></h6>
         </div>
         <div class="col-4">
-            <h5>Video presentación Álvaro Caro Jiménez</h5>
+            <h5>Video presentación renewablepowercapacities-stats</h5>
             <div class="embed-responsive embed-responsive-16by9 ">
                 <iframe
                     title="v1"
                     class="embed-responsive-item"
-                    src="https://www.youtube.com/watch?v=a_AvKGaSD8Ev"
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    src="https://www.youtube.com/embed/a_AvKGaSD8E"
                     allowfullscreen
                 />
             </div>
@@ -71,12 +76,17 @@
         margin-top: 2em;
         margin-right: 2em;
         margin-left: 2em;
+        text-align: justify; 
     }
     h4 {
         text-align: center;
         margin-top: 3em;
     }
     h5 {
+        text-align: center;
+        margin-bottom: 1em;
+    }
+    h6 {
         text-align: center;
         margin-bottom: 1em;
     }
